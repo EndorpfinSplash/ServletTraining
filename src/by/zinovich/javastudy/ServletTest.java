7a63ba37b40a083e3aeabaf3e745ad41bdd5de33
AZ <fast.optimus@gmail.com>
2018-01-25 17:00:51 +0300

    my servlet training

---
+++ src/by/zinovich/javastudy/ServletTest.java
@@ -0,0 +1,17 @@
+package by.zinovich.javastudy;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletTest extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().write("Hello world!");
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Andrey.Zinovich
+  Date: 25.01.2018
+  Time: 16:10
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  Index
+  </body>
+</html>
